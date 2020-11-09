eb3cb4e8f0dbc5b53875ba0da3dad5c77a197bcb
Esther Cortez <70614557+astridcortez@users.noreply.github.com>
2020-11-08 19:03:46 -0500

    Update SongTree.java

--- SongTree.java
+++ SongTree.java
@@ -17,39 +17,11 @@ public class SongTree {
     public static PrintStream ps;
     
     public static void main(String[] args) throws Exception{
-        /*ArrayList<String> songList = new ArrayList<>(); //stores all song titles
-        ArrayList<String> artistList = new ArrayList<>(); //stores all artist names
-        ArrayList<Integer>streamList = new ArrayList<>(); //stores all stream counts
-        */
+        
         ps = new PrintStream ("results.txt"); // sending all output to a file named results
         String [] fileNames = {"week1.txt", "week2.txt", "week3.txt",
                 "week4.txt"};
-        /*
-        for (int i = 0; i < fileNames.length ; i++){
-            String myFile = fileNames[i];
-            Scanner sc = new Scanner (new File(myFile)); // scanning files
-            while(sc.hasNextLine()){ // while there is things to scan
-                String line = sc.nextLine(); // scan next line
-                String[] array = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1); //gets rid of unnecessary quotes
-                if (array.length > 1) {
-                    for (int j = 0; j < array.length; j++) {
-                        if (array[j].charAt(0) == '"') {
-                            array[j] = array[j].substring(1, array[j].length() - 1);
-
-                        }
-                    }
-                    
-                    songList.add(array[1]);    
-                    artistList.add(array[2]);
-                    streamList.add(Integer.parseInt(array[3]));
-                    
-                }
-                
-            }
-            makeBinarySearchTree(songList, artistList, streamList); //make binary search tree
-            System.out.println(list.root);
-        }
-        */
+       
         int columns = 5;
         int rows = 800;
         int row=0;
@@ -70,10 +42,6 @@ public class SongTree {
         }
         makeBinarySearchTree(songList, artistList, streamList); //make binary search tree
         
-        
-            
- 
-            
     }
     
     //populates the songList array, artistList array, streamList array based on the fiels we have
@@ -93,35 +61,7 @@ public class SongTree {
                 for(int i = 0; i < 5; i++){
                 
                     myList[row][i] = values[i];                    
-                    //ps.print(myList[row][i] + " "); //prints out to csv file to another file
-                    //System.out.println(myList[row][i] + " ");
-                    //ps.println(myList[row][i]+ "");
-                    /*
-                    switch (i) {
-                        case 1:
-                            songList[row] = values [1];
-                            break;
-                        case 2:
-                            artistList[row] = values[2];
-                            break;
-                        case 3:
-                            streamList[row] = Integer.parseInt(values [3]);
-                            break;
-                        default:
-                            break;
-                    }
                     
-*/
-                    /*
-                    if(i == 1){
-                        songList[row] = values [1];
-                    }
-                    else if(i==2){
-                        artistList[row] = values[2];
-                    }
-                    else if (i == 3){
-                        streamList[row] = Integer.parseInt(values [3]);
-                    }*/
                 }
                 songList[row] = values [1]; //populates songList array
                 artistList[row] = values [2]; //populates the artistList array
